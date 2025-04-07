341152f7b187428fa347f28c803b7c179f62e934
paolo-jorge-soriano <182842652+paolo-jorge-soriano@users.noreply.github.com>
2025-04-07 21:46:14 +0800

    Add features: eraser mode, set grid size Change color scheme, font properties

--- script.js
+++ script.js
@@ -5,15 +5,18 @@ const DEFAULT_MODE = "color";
 
 const sketchContainer = document.getElementById("sketchContainer");
 const colorPicker = document.getElementById("color-picker");
-const btnResetGrid = document.getElementById("btn-reset-grid");
 const btnColorMode = document.getElementById("btn-color-mode");
 const btnRainbowMode = document.getElementById("btn-rainbow-mode");
+const btnEraserMode = document.getElementById("btn-eraser-mode");
+const btnResetGrid = document.getElementById("btn-reset-grid");
+const btnSetGridSize = document.getElementById("btn-set-grid-size");
 
 // Initialize default values
 let gridSize = DEFAULT_GRID_SIZE;
 let currentColor = DEFAULT_COLOR;
 let currentMode = DEFAULT_MODE;
 let isSketching = false;
+btnColorMode.classList.add("active-state");
 
 // FUNCTIONS
 function setColor() {
@@ -31,8 +34,11 @@ function setColor() {
         currentColor = randomColor;
     }
 
-    return currentColor;
+    else if (currentMode === "eraser") {
+        currentColor = "transparent";
+    }
 
+    return currentColor;
 }
 
 function setGrid(size) {
@@ -67,24 +73,55 @@ function resetGrid() {
 }
 
 // DOM
-btnColorMode.addEventListener("click", () => {
-    currentMode = "color";
-});
-
-btnRainbowMode.addEventListener("click", () => {
-    currentMode = "rainbow";
+document.addEventListener("mouseup", () => {
+    isSketching = false;
 });
 
 colorPicker.addEventListener("input", (e) => {
     currentColor = e.target.value;
 });
 
-document.addEventListener("mouseup", () => {
-    isSketching = false;
+btnColorMode.addEventListener("click", () => {
+    currentMode = "color";
+    btnColorMode.classList.add("active-state");
+    btnRainbowMode.classList.remove("active-state");
+    btnEraserMode.classList.remove("active-state");
+});
+
+btnRainbowMode.addEventListener("click", () => {
+    currentMode = "rainbow";
+    btnColorMode.classList.remove("active-state");
+    btnRainbowMode.classList.add("active-state");
+    btnEraserMode.classList.remove("active-state");
+});
+
+btnEraserMode.addEventListener("click", () => {
+    currentMode = "eraser";
+    btnColorMode.classList.remove("active-state");
+    btnRainbowMode.classList.remove("active-state");
+    btnEraserMode.classList.add("active-state");
 });
 
 btnResetGrid.addEventListener("click", () => {
     resetGrid();
+});
+
+btnSetGridSize.addEventListener("click", () => {
+    let userInput = prompt("Enter grid size: ");
+
+    // If user clicks cancel
+    if (userInput === null) {
+        return;
+    }
+
+    else if (Number.isInteger(Number(userInput)) && Number(userInput) > 0 && Number(userInput) <= 100) {
+        gridSize = parseInt(userInput);
+        resetGrid(gridSize);
+    }
+    
+    else {
+        alert("Please enter a number from 1 to 100 only.");
+    }
 });
 
 // Main
