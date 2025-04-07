17992a4f1684e5a961f20ce727e8e96f54d08b00
paolo-jorge-soriano <182842652+paolo-jorge-soriano@users.noreply.github.com>
2025-04-07 16:40:07 +0800

    Add features: color picker, reset grid

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-const sketchContainer = document.getElementById("sketchContainer");
-const gridSize = 16; // temporary only. convert to function later
+const DEFAULT_GRID_SIZE = 16;
 
+const sketchContainer = document.getElementById("sketchContainer");
+const colorPicker = document.getElementById("colorPicker");
+
+let gridSize = DEFAULT_GRID_SIZE;
+let currentColor = colorPicker.value;
 let isSketching = false;
 
 function setGrid(size) {
-    sketchContainer.innerHTML = "";
     const blockSize = 640 / size;
 
     for (let i = 0; i < size * size; i++) {
@@ -12,15 +15,17 @@ function setGrid(size) {
         block.classList.add("block");
         block.style.width = `${blockSize}px`;
         block.style.height = `${blockSize}px`;
+        block.setAttribute("draggable", false);
 
+        // Sketch feature
         block.addEventListener("mousedown", () => {
             isSketching = true;
-            block.style.backgroundColor = "black";
+            block.style.backgroundColor = currentColor;
         });
 
         block.addEventListener("mouseover", () => {
             if (isSketching) {
-                block.style.backgroundColor = "black";
+                block.style.backgroundColor = currentColor;
             }
         });
 
@@ -28,9 +33,18 @@ function setGrid(size) {
     }
 }
 
+function clearGrid() {
+    sketchContainer.innerHTML = "";
+    setGrid(gridSize);
+}
+
+colorPicker.addEventListener("input", (e) => {
+    currentColor = e.target.value;
+});
+
 document.addEventListener("mouseup", () => {
     isSketching = false;
 });
 
-// Main
-setGrid(gridSize);
+// Initialize default state
+setGrid(DEFAULT_GRID_SIZE);
